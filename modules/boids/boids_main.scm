87a6227ce277215d59d6db7db94fadabb2b5b59e
P_Pumulo <je.le.condor.oeuf@gmail.com>
2025-11-16 18:56:24 +0900

    マージできそう

--- modules/boids/boids_main.scm
+++ modules/boids/boids_main.scm
@@ -1,5 +1,9 @@
-(import chicken scheme)
-(use kak-decomposition)
+;; Load local core libraries for distribution builds where 'chicken'
+;; may not be available as an importable module.
+(load "../../core/kak_decomposition.scm")
+(load "../../core/machine_constants.scm")
+(load "../../core/cartan_utils.scm")
+
 
 (define (main args)
   (display "Boids module\n"))
--- modules/fmm/fmm_main.scm
+++ modules/fmm/fmm_main.scm
@@ -1,5 +1,8 @@
-(import chicken scheme)
-(use kak-decomposition)
+;; Load local core libraries for distribution builds where 'chicken'
+;; may not be available as an importable module.
+(load "../../core/kak_decomposition.scm")
+(load "../../core/machine_constants.scm")
+(load "../../core/cartan_utils.scm")
 
 (define (main args)
   (display "FMM module\n"))
