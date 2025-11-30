01b606769a8b0b0a5321e86cfd3575b6a85e3236
Yoshihiro Hasegawa <je.le.condor.oeuf@gmail.com>
2025-11-30 21:11:58 +0900

    少し済んだがまだフロンティア関係がある

--- core/cartan_utils.scm
+++ core/cartan_utils.scm
@@ -5,13 +5,14 @@
 (module cartan-utils
   (pretty-print-decomposition
    validate-decomposition
-   cartan-log-decompose)          ;; エクスポート
+   cartan-log-decompose)
 
   (import scheme)
   (import (chicken base)
-          (chicken format)        ;; printf を使うために必要
-          srfi-1                  ;; iota, map
-          machine-constants)      ;; safe-log 等を利用 (ローカルモジュール)
+          (chicken format)
+          srfi-1
+          srfi-13          ;; string-pad のために必要
+          machine-constants)
 
   ;; Validate decomposition parameters
   (define (validate-decomposition B steps)
@@ -20,21 +21,24 @@
          (integer? steps)))
 
   ;; Compute log-spaced Cartan decomposition levels
-  ;; (cartan-log-decompose B steps) => list of length (steps+1)
-  ;; uses safe-log for numeric safety
   (define (cartan-log-decompose B steps)
     (let ((lnB (safe-log B)))
       (map (lambda (k)
              (exp (/ (* k lnB) steps)))
            (iota (+ steps 1)))))
 
-  ;; Pretty print decomposition hierarchy (uses cartan-log-decompose)
+  ;; Pretty print decomposition hierarchy
+  ;; FIX: Use Chicken Scheme's format syntax
   (define (pretty-print-decomposition B steps)
     (let ((decomp (cartan-log-decompose B steps)))
-      (printf "Cartan decomposition (B=~a, steps=~a):~n" B steps)
+      (printf "Cartan decomposition (B=~a, steps=~a):~%" B steps)
       (for-each (lambda (k v)
-                  (printf "  Level ~2d: ~10,6f~n" k v))
+                  ;; Chicken Scheme format: ~a (any), ~s (string), ~% (newline)
+                  ;; Use string formatting for precise control
+                  (printf "  Level ~a: ~a~%" 
+                          (string-pad (number->string k) 2 #\space)
+                          (string-pad (number->string v) 10 #\space)))
                 (iota (+ steps 1))
-                decomp)))
+                decomp))))
 
-  ) ;; end module
+   ;; end module
