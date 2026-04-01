19e1a4b614df9be6f86df3a5145a202abd61c741
Yoshihiro Hasegawa <je.le.condor.oeuf@gmail.com>
2026-04-01 14:00:08 +0900

    名前空間名とAuthorsをなぜか統一してた

--- dist-proof/lean4/HatsuYakitori/ExteriorAnalysis.lean
+++ dist-proof/lean4/HatsuYakitori/ExteriorAnalysis.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2026 HatsuYakitori. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: HatsuYakitori
+Authors: Yoshihiro Hasegawa
 -/
 import Mathlib.Data.Real.Basic
 import Mathlib.Analysis.SpecialFunctions.Log.Basic
