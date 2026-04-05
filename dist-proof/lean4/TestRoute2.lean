354f73a4d6fb3a2aa67d2e193c75153f0a6cd528
Yoshihiro Hasegawa <je.le.condor.oeuf@gmail.com>
2026-04-06 00:04:49 +0900

    カラビナ本体のsorryを二つ潰した

--- dist-proof/lean4/TestRoute2.lean
+++
@@ -1,7 +0,0 @@
-import Mathlib
-import HatsuYakitori.Carabiner
-
-theorem test : (golayRoute.complement).isAscending := by
-  unfold Route.isAscending Route.complement golayRoute
-  dsimp [carabiner0, carabiner8, carabiner12, carabiner16, carabiner24, Carabiner.complement, Carabiner.height, GolayWeight.complement, GolayWeight.height, GolayWeight.toFin25, octadHeight, galoisHeightBound]
-  decide
--- dist-proof/lean4/TestRoute3.lean
+++
@@ -1,5 +0,0 @@
-import Mathlib
-import HatsuYakitori.Carabiner
-
-theorem test : (golayRoute.complement).isAscending := by
-  sorry
