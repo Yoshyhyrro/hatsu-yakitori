7485ad1e3339a92da8d0ff2039e9724ef88a6b98
Yoshihiro Hasegawa <je.le.condor.oeuf@gmail.com>
2026-07-12 23:01:46 +0900

    leanOptions := #[

--- dist-proof/lean4/lakefile.lean
+++ dist-proof/lean4/lakefile.lean
@@ -5,11 +5,13 @@ package «hatsu-yakitori-proof» where
   version := v!"0.1.0"
 
   leanOptions := #[
-    ⟨`diagnostics, true⟩,
+    ⟨`diagnostics, false⟩,
     ⟨`pp.proofs, false⟩,
     ⟨`pp.deepTerms, false⟩,
+    ⟨`pp.universes, false⟩,
+    ⟨`pp.fullNames, false⟩,
     ⟨`synthInstance.maxHeartbeats, 3000⟩
-  ]
+]
 
 require mathlib from git
   "https://github.com/leanprover-community/mathlib4.git"
