09122c90ca701ac72f7917080b56ed6a425802d9
Yoshihiro Hasegawa <je.le.condor.oeuf@gmail.com>
2026-07-12 21:22:59 +0900

    ⟨pp.proofs, false⟩,⟨pp.motives, false⟩,⟨pp.maxSteps, 500⟩,⟨synthInstance.maxHeartbeats, 3000⟩

--- dist-proof/lean4/lakefile.lean
+++ dist-proof/lean4/lakefile.lean
@@ -5,12 +5,11 @@ package «hatsu-yakitori-proof» where
   version := v!"0.1.0"
 
   leanOptions := #[
-    ⟨`pp.explicit, true⟩,
-    ⟨`pp.universes, true⟩,
-    ⟨`pp.analyze, true⟩,
-    ⟨`pp.coercions, true⟩,
     ⟨`diagnostics, true⟩,
-    ⟨`weak.trace.Meta.synthInstance, true⟩
+    ⟨`pp.proofs, false⟩,
+    ⟨`pp.motives, false⟩,
+    ⟨`pp.maxSteps, 500⟩,
+    ⟨`synthInstance.maxHeartbeats, 3000⟩
   ]
 
 require mathlib from git
