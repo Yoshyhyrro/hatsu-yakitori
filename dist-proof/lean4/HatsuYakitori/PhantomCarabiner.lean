2976642bc5b52eb8c74944474fa8d7af34c94bab
Yoshihiro Hasegawa <je.le.condor.oeuf@gmail.com>
2026-07-13 23:43:13 +0900

    ake build Hatsuyakitori.QuaternionCarabiner^M

--- dist-proof/lean4/HatsuYakitori/PhantomCarabiner.lean
+++ dist-proof/lean4/HatsuYakitori/PhantomCarabiner.lean
@@ -1,6 +1,7 @@
 /-
-Copyright (c) 2026 hatsu-yakitori contributors. All rights reserved.
+Copyright (c) 2026 HatsuYakitori. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Yoshihiro Hasegawa
 -/
 
 import HatsuYakitori.Carabiner
